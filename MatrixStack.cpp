853074ad9cca4a4468323e86cc8618f4405fb091
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-12-17 14:46:52 -0600

    Imp. MatrixStack (Not tested)
    Implemented MatrixStack, but have not tested it
    yet.

--- MatrixStack.cpp
+++ MatrixStack.cpp
@@ -3,17 +3,30 @@
 MatrixStack::MatrixStack()
 {
 	// Initialize the matrix stack with the identity matrix.
+	m_matrices.push_back(Matrix4f::identity());
 }
 
 void MatrixStack::clear()
 {
 	// Revert to just containing the identity matrix.
+	m_matrices.clear();
+	m_matrices.push_back(Matrix4f::identity());
 }
 
 Matrix4f MatrixStack::top()
 {
 	// Return the top of the stack
-	return Matrix4f();
+
+	// 'Top' matrix (T)
+	Matrix4f T = Matrix4f::identity();
+
+	// Multiple matrices in stack to get 'Top' matrix
+	for (const Matrix4f& M : m_matrices)
+	{
+		T = T * M;
+	}
+
+	return T;
 }
 
 void MatrixStack::push( const Matrix4f& m )
@@ -21,9 +34,13 @@ void MatrixStack::push( const Matrix4f& m )
 	// Push m onto the stack.
 	// Your stack should have OpenGL semantics:
 	// the new top should be the old top multiplied by m
+
+	m_matrices.push_back(m);
 }
 
 void MatrixStack::pop()
 {
 	// Remove the top element from the stack
+
+	m_matrices.pop_back();
 }
