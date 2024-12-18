675d6ececea9a9a3967f2d24f959e4c0e3617968
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-12-17 20:14:22 -0600

    Drawing skeleton (has MAJOR bug)
    will fix bug next

--- SkeletalModel.cpp
+++ SkeletalModel.cpp
@@ -137,9 +137,95 @@ void SkeletalModel::drawJoints( )
 	}
 }
 
+void drawSkeletonHelper(const Joint* joint, MatrixStack& stack, const Vector3f& parentOffset)
+{
+	// The offset is distance between child (current) and parent, and 
+	// it points to the parent from the child
+
+	// set up joint frame
+	stack.push(joint->transform);
+
+	// draw box
+		// this is a bit complicated, but:
+
+		// add cube transformations
+		// draw the cube
+		// pop cube transformations
+	
+	// not root
+	if (parentOffset != Vector3f(0,0,0))
+	{
+		// draw the bone (actually a stretched cube)
+
+		// Drawing the stretched cube is a bit complicated due 
+		// to glut only drawing cubes centered at the origin.
+		// Thus, the coordinate system needs to be transformed
+		// to correctly place the cube/bone. 
+
+		// arbitrary vector for finding x, and y
+		const Vector3f rnd(0,0,1);
+
+		const Vector3f z = parentOffset.normalized();
+		const Vector3f y = Vector3f::cross(z,rnd).normalized();
+		const Vector3f x = Vector3f::cross(y,z).normalized();
+
+		// Rotation (R)
+		// Rotates basis so that z points in same direction as parent offset
+		stack.push(Matrix4f(
+			x.x(), y.x(), z.x(), 0,
+			x.y(), y.y(), z.y(), 0,
+			x.z(), y.z(), z.z(), 0,
+			0,     0,     0,     1
+		));
+
+		// Scalar (S)
+		// Scale z axis by half the parent offset
+		stack.push(Matrix4f(
+			1, 0, 0,                    0,
+			0, 1, 0,                    0,
+			0, 0, parentOffset.abs()/2, 0,
+			0, 0, 0,                    1
+		));
+
+		// Translation (T)
+		// Translate up z axis by 1
+		stack.push(Matrix4f(
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 1,
+			0, 0, 0, 1
+		));
+
+		// R * S * T will transform the frame, so that 
+		// the bone (stretched cube) will be drawn correctly between
+		// this joint and the parent joint.
+		
+		// set special frame for drawing bone
+		glLoadMatrixf(stack.top().getElements());
+
+		// draw the bone
+		glutSolidCube(1.0f);
+
+		// pop special transformations for drawing bone
+		stack.pop(); // T
+		stack.pop(); // S
+		stack.pop(); // R
+	}
+
+	// Draw children
+	for (const Joint* child : joint->children)
+	{
+		drawSkeletonHelper(child, stack, -(child->transform.getCol(3).xyz()));
+	}
+
+	// pop joint frame
+	stack.pop();
+}
+
 void SkeletalModel::drawSkeleton( )
 {
 	// Draw boxes between the joints. You will need to add a recursive helper function to traverse the joint hierarchy.
+	drawSkeletonHelper(m_rootJoint, m_matrixStack, Vector3f(0,0,0));
 }
 
 void SkeletalModel::setJointTransform(int jointIndex, float rX, float rY, float rZ)
